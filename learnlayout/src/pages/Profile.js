c3d4e69e4df4cc3c44dd9e169e6b2ea809afbc8e
lucasvoron <72956673+lucasvoron@users.noreply.github.com>
2023-06-07 18:36:35 -0700

    Profile Sliders
    yahga

--- learnlayout/src/pages/Profile.js
+++ learnlayout/src/pages/Profile.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {Link} from "react-router-dom";
 import linkedIn from "../assets/linkedin_profile.png";
 import edit_profile from "../assets/edit_profile.png";
 import pastIcon from "../assets/pastIcon.png"
 
 const Profile = () => {
+  const [visual, setVisual] = useState(60);
+  const [reading, setReading] = useState(25);
+  const [auditory, setAuditory] = useState(15);
+  const [username, setUsername] = useState("LUCAS VORON");
+  const [email, setEmail] = useState("lvoron@scu.edu");
+
+  useEffect(() => {
+    console.log("visual has changed:", visual);
+    console.log("reading/writing has changed:", reading);
+    console.log("auditory has changed:", auditory);
+    console.log("username has changed:", username);
+    console.log("email  has changed:", email);
+
+    // handleSubmit();
+    // console.log("that has been called");
+  }, [visual, reading, auditory, username, email]);
+
   return (
     <div>
     <div className="landing-content-area">
@@ -12,11 +29,28 @@ const Profile = () => {
         <div className="landing-left">
           <div className="landing-left-container">
             <div className="landing-top-content">
-              <div className="landing-name-title">LUCAS VORON</div>
-              <div className="landing-subtitle">lvoron@scu.edu</div>
+              <div className="landing-name-title">{username}</div>
+              <div className="landing-subtitle">{email}
+                <div className="edit-icon-container">
+                  <img src={edit_profile} className="edit-icon" alt="Edit Profile" />
+                </div>
+              </div>
             </div>
             <div className="landing-middle-content">
-              <div className="landing-top-title">Visual - 60%</div>
+              <div className="landing-top-title">Visual: {visual}%
+                <div className="slidecontainer">
+                  <input
+                    type="range"
+                    min="0"
+                    max="100"
+                    defaultValue="60"
+                    className="slider"
+                    id="myRange"
+                    step="1"
+                    onChange={(event) => setVisual(event.target.value)}
+                  ></input>
+                </div>
+              </div>
               <div className="landing-top-text">What We Recommend:<br></br>
                 - heavily using charts, graphs, and pictures when learning new content <br></br>
                 - highlighting key points and terms <br></br>
@@ -24,11 +58,37 @@ const Profile = () => {
                 - in the classroom, make a conscious effort to see the lecturer </div>
             </div>
             <div className="landing-middle-content">
-              <div className="landing-top-title">Reading - 25%</div>
+              <div className="landing-top-title">Reading: {reading}%
+                  <div className="slidecontainer">
+                    <input
+                      type="range"
+                      min="0"
+                      max="100"
+                      defaultValue="25"
+                      className="slider"
+                      id="myRange"
+                      step="1"
+                      onChange={(event) => setReading(event.target.value)}
+                    ></input>
+                  </div>
+                </div>
               <div className="landing-top-text">For your learning style, we recommend using a combination or reading and visual learning styles to get the most out of material. Based on your information, our analytics recommend reading content and taking breaks in between to visually draw out what you understand.</div>
             </div>
             <div className="landing-bottom-content">
-              <div className="landing-top-title">Auditory - 15%</div>
+                <div className="landing-top-title">Auditory: {auditory}%
+                  <div className="slidecontainer">
+                    <input
+                      type="range"
+                      min="0"
+                      max="100"
+                      defaultValue="15"
+                      className="slider"
+                      id="myRange"
+                      step="1"
+                      onChange={(event) => setAuditory(event.target.value)}
+                    ></input>
+                  </div>
+                </div>
               <div className="landing-top-text">This is the most popular learning style. Because you rank this so low based on your quiz results, we recommend to always use this in conjunction with another technique. For example, taking notes in a lecture or drawing out what you recall visually afterwards. </div>
             </div>
           </div>
